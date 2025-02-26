0f905793d6c71ec5a046abf0c429308225f04102
ThimiraMadusanka <pdt.madusanka@gmail.com>
2025-02-26 12:13:57 +0530

    version 1.0

---
+++ src/data/FAQList.js
@@ -0,0 +1,23 @@
+// FAQ List
+export const FAQList = [
+    {
+        question: "What services does Lumin Solutions offer?",
+        answer: "We offer a comprehensive range of marketing services, including Digital Marketing (SEO, PPC, Social Media Marketing, Content Marketing), Branding (Brand Strategy, Logo Design, Brand Identity), Creative Services (Graphic Design, Video Production, Photography), and Web Development (Website Design, E-commerce Solutions, UX/UI Design)."
+    },
+    {
+        question: "How does Lumin Solutions approach a new project?",
+        answer: "We offer a comprehensive range of marketing services, including Digital Marketing (SEO, PPC, Social Media Marketing, Content Marketing), Branding (Brand Strategy, Logo Design, Brand Identity), Creative Services (Graphic Design, Video Production, Photography), and Web Development (Website Design, E-commerce Solutions, UX/UI Design)."
+    },
+    {
+        question: "Can you help improve our existing marketing strategies?",
+        answer: "We offer a comprehensive range of marketing services, including Digital Marketing (SEO, PPC, Social Media Marketing, Content Marketing), Branding (Brand Strategy, Logo Design, Brand Identity), Creative Services (Graphic Design, Video Production, Photography), and Web Development (Website Design, E-commerce Solutions, UX/UI Design)."
+    },
+    {
+        question: "What industries does Lumin Solutions specialize in?",
+        answer: "We offer a comprehensive range of marketing services, including Digital Marketing (SEO, PPC, Social Media Marketing, Content Marketing), Branding (Brand Strategy, Logo Design, Brand Identity), Creative Services (Graphic Design, Video Production, Photography), and Web Development (Website Design, E-commerce Solutions, UX/UI Design)."
+    },
+    {
+        question: "How long does it take to see results from a marketing campaign?",
+        answer: "We offer a comprehensive range of marketing services, including Digital Marketing (SEO, PPC, Social Media Marketing, Content Marketing), Branding (Brand Strategy, Logo Design, Brand Identity), Creative Services (Graphic Design, Video Production, Photography), and Web Development (Website Design, E-commerce Solutions, UX/UI Design)."
+    },
+]
---
+++ src/data/WhatWeDoList.js
@@ -0,0 +1,35 @@
+// What we do List
+export const WhatWeDoList = [
+    {
+        idName: "Digital_Marketing",
+        title: "Digital Marketing",
+        description: "At Lumin Solutions, we harness the power of digital marketing to elevate your brand and connect you with your target audience. Our comprehensive digital marketing services are designed to drive traffic, increase engagement, and boost conversions.",
+        labels: ["SEO", "Social Media Marketing", "Content Marketing"],
+        images: [
+            "https://img.freepik.com/free-photo/top-view-cloud-with-word-seo_1134-68.jpg?t=st=1740542574~exp=1740546174~hmac=c869398abc8471c32d86af3bc81cd7859ccd4829ab7850fe9e147b75a014a367&w=1380",
+            "https://img.freepik.com/free-photo/hands-holding-smartphone-social-media-concept_23-2150208239.jpg?t=st=1740544295~exp=1740547895~hmac=293255b987128d8819d4f1c4a277bc001f35401a5558a2dad621d55f6d4d8568&w=1380",
+            "https://img.freepik.com/free-photo/media-marketing-internet-digital-global_53876-138500.jpg?t=st=1740544437~exp=1740548037~hmac=fbecd43dddf52df781be4349725ca3e525b08866a17a5428948710943d5cc892&w=1380"
+        ],
+    },
+    {
+        idName: "Branding",
+        title: "Branding",
+        description: "At Lumin Solutions, we understand that a strong brand is the foundation of any successful business. Our branding services are designed to create a memorable and impactful brand identity that resonates with your target audience and sets you apart from the competition.",
+        labels: ["Logo Design", "Brand Identity"],
+        images: [
+            "https://img.freepik.com/free-photo/hipster-young-illustrator-artist-black-simple-tshirt-creates-authentic-unique-hand-lettering-drawing-his-bright-industrial-studio_346278-332.jpg?t=st=1740545439~exp=1740549039~hmac=6bf0173cb77c4cc54ba9af64474dbe7c16250a4c2cf99d49c491264f1aa9d63b&w=1380",
+            "https://img.freepik.com/free-photo/top-view-working-tools_1134-67.jpg?t=st=1740545552~exp=1740549152~hmac=d14176ea67dd5fc1b21c31dc7fdfb7d11f35ffe5b735d76b1006c00033a3795a&w=1380",
+            "https://img.freepik.com/free-photo/online-marketing_53876-95312.jpg?t=st=1740545536~exp=1740549136~hmac=19c04356b0b04b389d4e10c600bdea8e1db0f64142ade3beb233df5cff4d310c&w=1380"
+        ],
+    },
+    {
+        idName: "Creative_Services",
+        title: "Creative Services",
+        description: "At Lumin Solutions, our Creative Services team is dedicated to bringing your vision to life with innovative and captivating designs. We believe that creativity is at the heart of effective marketing, and we strive to create visual content that not only captures attention but also drives engagement and results.",
+        labels: ["Graphic Design", "Photography"],
+        images: [
+            "https://img.freepik.com/free-photo/inspiration-ideas-design-creative-thinking-word_53876-92924.jpg?t=st=1740545758~exp=1740549358~hmac=a6ce5847368f852cdd307cc1c3bf3c97a590f21f299462405efb92d160c22b81&w=1380",
+            "https://img.freepik.com/free-photo/bearded-hipster-amateur-photographer-with-tattoos-arms-dressed-fleece-shirt-holds-compact-dslr-camera-grey-background_613910-15126.jpg?t=st=1740545648~exp=1740549248~hmac=3173731c5c8778e41341981c24fb00c2e4d54c661ba9eebfcb87327f87de0aa8&w=1380"
+        ],
+    },
+]
